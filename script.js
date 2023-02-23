c9732666aa789907b8ab866b5028ab8ca77558a9
Jetson14 <michaeljetson1402@gmail.com>
2023-02-23 13:09:14 +0530

    displaying todolist

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-const taskInput = document.querySelector(".task-input input");
+const taskInput = document.querySelector(".task-input input"),
+taskBox = document.querySelector(".task-box");
+
+// getting localstorage todo-list
+let todos = JSON.parse(localStorage.getItem("todo-list"));
+
+function showTodo(){
+    let li = "";
+    todos?.forEach((todo, id) => {
+        li += `<li class="task">
+                    <label for="${id}">
+                        <input type="checkbox" id="${id}">
+                        <p>${todo.name}</p>
+                    </label>
+                    <div class="settings">
+                        <i class="uil uil-ellipsis-h"></i>
+                        <ul class="task-menu">
+                            <li><i class="uil uil-pen"></i>Edit</li>
+                            <li><i class="uil uil-trash"></i>Delete</li>
+                        </ul>
+                    </div>
+                </li>`;
+    });
+    taskBox.innerHTML = li;
+}
+
+showTodo()
 
 taskInput.addEventListener("keyup", e => {
     let userTask = taskInput.value.trim();
     if(e.key == "Enter" && userTask){
-        // getting localstorage todo-list
-        let todos = JSON.parse(localStorage.getItem("todo-list"));
         if(!todos){ // if todos doesn't exist, pass an empty aray.
             todos = [];
         }
@@ -12,6 +36,6 @@ taskInput.addEventListener("keyup", e => {
         let taskInfo = {name: userTask, status:"pending"};
         todos.push(taskInfo); // adding new task to todos.
         localStorage.setItem("todo-list", JSON.stringify(todos)); 
-        //
+        showTodo();
     }
 })
